8495d1f2cc98c0f4e1e3f2ac0edc47d0d570c329
JacobHilse <jhilse@uw.edu>
2022-11-21 18:46:40 -0800

    firstCommitA4

--- source/analysis.R
+++ source/analysis.R
@@ -1,66 +1,126 @@
 library(tidyverse)
 
-# The functions might be useful for A4
-source("../source/a4-helpers.R")
-
-## Test queries ----
-#----------------------------------------------------------------------------#
-# Simple queries for basic testing
-#----------------------------------------------------------------------------#
-# Return a simple string
-test_query1 <- function() {
-  return ("Hello world")
-}
-
-# Return a vector of numbers
-test_query2 <- function(num=6) {
-  v <- seq(1:num)
-  return(v)
-}
-
 ## Section 2  ---- 
 #----------------------------------------------------------------------------#
-# Your functions and variables might go here ... <todo: update comment>
-#----------------------------------------------------------------------------#
+#In this section is establish my main dataframe as well as some important nationwide statistics
+
+dataMain <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+
+popStart <- dataMain %>%
+  filter(year == 1990) 
+popNew <- dataMain %>%
+  filter(year==2018)
+
+blackPropNew <- (100*(sum(popNew$black_jail_pop, na.rm = TRUE)/ sum(popNew$total_jail_pop, na.rm = TRUE)))
+blackPropStar <- (100*(sum(popStart$black_jail_pop, na.rm = TRUE)/ sum(popStart$total_jail_pop, na.rm = TRUE)))
+percTotalPop <- (100*(sum(popNew$total_jail_pop, na.rm = TRUE)/sum(popStart$total_jail_pop, na.rm = TRUE))-1)
 
 ## Section 3  ---- 
 #----------------------------------------------------------------------------#
 # Growth of the U.S. Prison Population
-# Your functions might go here ... <todo:  update comment>
-#----------------------------------------------------------------------------#
-# This function ... <todo:  update comment>
+
+#This function takes a subset of specific columns intended for use and returns them grouped and summarized
 get_year_jail_pop <- function() {
-  # TODO: Implement this function 
-return()   
+  subData <- dataMain[c(2,21)]
+  tempdf <- subData %>%
+    group_by(year)%>%
+    summarize(sumPop = sum(total_jail_pop, na.rm = TRUE))
+  return(tempdf)   
 }
 
-# This function ... <todo:  update comment>
+# This function plots the wrangled dataframe
 plot_jail_pop_for_us <- function()  {
-  # TODO: Implement this function 
-  return()   
+  plotdf <- get_year_jail_pop()
+  barchart <- ggplot(data=plotdf, aes(x=year, y=sumPop)) + geom_bar(stat = "identity") +
+    xlab("Year")+ylab("Total Incarcerated Individuals")
+  return(barchart)   
 } 
 
 ## Section 4  ---- 
 #----------------------------------------------------------------------------#
 # Growth of Prison Population by State 
-# Your functions might go here ... <todo:  update comment>
-# See Canvas
-#----------------------------------------------------------------------------#
+
+#Function that filters by vector(states) and then groups by the columns intended for use
+get_jail_pop_by_states <- function(states) {
+    fortemp <- dataMain %>%
+      filter(state %in%  states) %>%
+      group_by(state,year) %>%
+      summarize(stateSum = sum(total_jail_pop, na.rm=TRUE))
+  return(fortemp)
+}
+
+#Function plots the wrangled dataframe
+plot_jail_pop_by_states <- function(states) {
+  lineplotdf <- get_jail_pop_by_states(states)
+  lineReturn <- ggplot(data=lineplotdf, aes(x=year, y=stateSum, group=state)) +
+    geom_line(aes(linetype=state))+
+    geom_point() + xlab("Year")+ylab("Total Incarcerated Individuals by State")
+  return(lineReturn)
+}
 
 ## Section 5  ---- 
 #----------------------------------------------------------------------------#
-# <variable comparison that reveals potential patterns of inequality>
-# Your functions might go here ... <todo:  update comment>
-# See Canvas
-#----------------------------------------------------------------------------#
+
+#function returns a dataframe comparing the proportion of incarcerated white/black individuals to total incarcerated pop
+comparisonDF <- function(){
+  compTemp <- dataMain[c(2,21,29)]
+  compDF1 <- compTemp %>%
+    filter(year >= 1990) %>%
+    group_by(year)%>%
+    summarize(bProp = (sum(black_jail_pop, na.rm=TRUE)/sum(total_jail_pop, na.rm=TRUE)))
+    
+  compTemp <- dataMain[c(2,21,32)]
+  compDF2 <- compTemp %>%
+    filter(year >= 1990) %>%
+    group_by(year)%>%
+    summarize(wProp = (sum(white_jail_pop, na.rm=TRUE)/sum(total_jail_pop, na.rm=TRUE)))
+  
+  compTemp <- left_join(compDF1, compDF2, by="year")
+  return(compTemp)
+}
+
+#plot out my wrangled dataframe with specific visual elements
+plotComparison <- function(){
+  compplotdf <- comparisonDF()
+  compPlot <- ggplot(data=compplotdf, aes(year)) +
+    geom_line(aes(y=wProp, color="Proportion of White individuals"))+
+    geom_line(aes(y=bProp, color="Proportion of Black individuals"))+
+    xlab("Year")+ylab("Proportion of total Jail Population")+
+    guides(color=guide_legend(title="Chart Legend"))
+  
+  return(compPlot)
+}
 
 ## Section 6  ---- 
 #----------------------------------------------------------------------------#
-# <a map shows potential patterns of inequality that vary geographically>
-# Your functions might go here ... <todo:  update comment>
-# See Canvas
-#----------------------------------------------------------------------------#
+library(leaflet)
 
-## Load data frame ---- 
+#pull a csv that has lat,lng associated by fips
+coord <- read.csv("https://gist.githubusercontent.com/russellsamora/12be4f9f574e92413ea3f92ce1bc58e6/raw/3f18230058afd7431a5d394dab7eeb0aafd29d81/us_county_latlng.csv")
+coord$fips <- coord$fips_code
+coord <- coord[3:5]
+#Join the lat,lng info to a new dataframe so that I can plot on leaflet map
+coordMain <- left_join(dataMain, coord, by="fips")
 
+#Wrangling function : I create a df taking specific columns (including lat,lng)
+mapFrame <- function(){
+  mapDf <- coordMain[c(2,3,21,29,122,123)]
+  mapDf$bProp <- (mapDf$black_jail_pop / mapDf$total_jail_pop)
+  retdf <- mapDf %>%
+    filter(year==2018) %>%
+    select(lng,lat,bProp,fips)
+  
+  return(retdf)
+}
+
+#retun my subsetted df into my mapping function and create a map based off the leafdf$bProp statistic
+printMap <- function(){
+  leafdf <- mapFrame()
+  leafdf <- filter(leafdf, bProp <= 1)
+  mapOne <- leaflet(data=leafdf) %>%
+    addTiles() %>%
+    addCircleMarkers(lat=leafdf$lat, lng=leafdf$lng, popup = leafdf$bProp, stroke=FALSE, radius=(leafdf$bProp*7), fillOpacity = 0.5)
+  
+  return(mapOne)
+}
 
